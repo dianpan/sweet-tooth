6d7eb580601117768d29d49ee7782da3cf0483c7
Dian Pan <dian.di.pan@gmail.com>
2016-01-23 11:53:23 -0800

    add react build to config environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  MyApp::Application.configure do
+    config.react.variant = :production
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
