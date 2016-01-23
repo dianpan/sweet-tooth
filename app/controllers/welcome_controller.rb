306be95a7d210da37d2237a4288572dea5cf6c22
Jerry Chai <jchai002@gmail.com>
2016-01-23 14:45:25 -0800

    fix merge conflicts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-<<<<<<< HEAD
-    @search =
-=======
     @search= Search.new
-  end
-
-  def create
-    session[:long]=params[:search][:long]
-    session[:lat]=params[:search][:lat]
-    binding.pry
-    redirect_to root_path
->>>>>>> get rid of binding.pry
   end
 end
