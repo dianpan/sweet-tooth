66c3d7d724e633fb374dcf7bbed3aeefcefd1341
Jerry Chai <jchai002@gmail.com>
2016-01-23 17:19:05 -0800

    adjust main component

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -33,7 +33,7 @@ var Main = React.createClass({
     return (
     <div>
       <button name="button" onClick={this.setLocation}>Button</button>
-      <Map />
+      <button name="button" onClick={this.showMap}>Button2</button>
     </div>
     )
   },
@@ -53,5 +53,44 @@ var Main = React.createClass({
        }
      }
     console.log(this.state.data)
+  },
+  showMap: function(){
+        var lng = $("#user_long");
+      	var lat = $("#user_lat");
+
+        function getLocation() {
+
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(setPosition);
+      	    }
+      	   }
+
+      	function setPosition(position) {
+      	    lat.val(position.coords.latitude)
+      	    lng.val(position.coords.longitude)
+      	    var latitude = parseFloat(lat.val())
+      	    var longitude = parseFloat(lng.val())
+
+      		initMap(longitude,latitude)
+
+      	}
+
+      	function initMap(longitude,latitude) {
+      		var mapCenter = {lat: latitude, lng: longitude}
+      	    var mapOptions = {
+      	      zoom: 16,
+      	      center: mapCenter,
+      	      scrollwheel: false,
+      	      draggable:false
+      	    }
+      	    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      	    var marker = new google.maps.Marker({
+      	    position: mapCenter,
+      	    map: map
+      	  });
+      	}
+      getLocation();
+
   }
 });
