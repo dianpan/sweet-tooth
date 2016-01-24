adefab4dff2f86e28449a06eba60dab270f0b210
Jerry Chai <jchai002@gmail.com>
2016-01-23 17:09:18 -0800

    fix merge conf

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -1,13 +1,50 @@
 var Main = React.createClass({
-  render: function(){
-    // var shops = this.props.data.map(function(shop){
-    //   return <Shop {...shop} />
-    // });
+  getInitialState: function(){
+    return {
+      data:null,
+      longitude:'',
+      latitude:''
+    }
+  },
+  sendData: function(){
+       var formData = {
+         location: {
+           longitude:this.state.longitude,
+           latitude:this.state.latitude
+         }
+       }
 
+       console.log(formData)
+      //    $.ajax({
+      //    url: "/searches",
+      //    dataType: 'json',
+      //    type: 'POST',
+      //    data: formData,
+      //    success: function(data) {
+      //      console.log(data)
+      //    }.bind(this),
+      //    error: function(xhr, status, err) {
+      //      console.error( status, err.toString());
+      //    }.bind(this)
+      //  });
+  //  },
+  },
+  render: function(){
     return (
     <div>
-      hshs
+      <button name="button" onClick={this.setLocation}>Button</button>
     </div>
     )
+  },
+  setLocation: function(){
+      var long=$("#user_long").val()
+      var lat=$("#user_lat").val()
+      this.setState({longitude:long})
+      this.setState({latitude:lat})
+  },
+  componentDidUpdate: function(){
+    if (this.state.longitude && this.state.latitude) {
+      this.sendData();
+    }
   }
 });
